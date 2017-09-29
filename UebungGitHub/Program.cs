7614ac6e5e2d1a4353f078635aaf3a73b4e87cb2
mario.trappl <mario.trappl@gmail.com>
2017-09-29 11:27:24 +0200

    First commit

--- UebungGitHub/Program.cs
+++ UebungGitHub/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UebungGitHub
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //erster Eintrag
-            int version = 0;
-        }
+      //erster Eintrag
+      int version = 0;
+
+      for (;;)
+      {
+        Debug.WriteLine("Test");
+      }
+
     }
+  }
 }
